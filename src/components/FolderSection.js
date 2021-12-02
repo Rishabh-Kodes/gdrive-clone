575875bba96147366aa8a007b2214f49af468ac6
Rishabh-Kodes <rishabhkodes@gmail.com>
2021-12-02 23:40:20 +0530

    Added create folder modal

--- src/components/FolderSection.js
+++ src/components/FolderSection.js
@@ -1,21 +1,61 @@
+import { useState } from "react";
 import Folder from "./Folder";
 
 const FolderSection = (props) => {
+  const [showCreateFolder, setShowCreateFolder] = useState(true);
+  const [folderName, setFolderName] = useState("");
+
+  const handleCancel = () => {
+    setShowCreateFolder(false);
+  };
+
+  const handelCreate = () => {
+    console.log(folderName);
+    setShowCreateFolder(false);
+  };
+
   return (
     props.folders && (
-      <section>
-        <div className="section-header">
-          <h3 className="section-title">Folders</h3>
+      <>
+        <section>
+          <div className="section-header">
+            <h3 className="section-title">Folders</h3>
 
-          <button>Create Folder</button>
-        </div>
+            <button onClick={() => setShowCreateFolder(true)}>
+              Create Folder
+            </button>
+          </div>
 
-        <div className="grid-6">
-          {props.folders.map((item, index) => (
-            <Folder data={item} key={index} />
-          ))}
-        </div>
-      </section>
+          <div className="grid-6">
+            {props.folders.map((item, index) => (
+              <Folder data={item} key={index} />
+            ))}
+          </div>
+        </section>
+
+        {showCreateFolder && (
+          <div className="modal">
+            <div className="create-folder">
+              <div className="title">New folder</div>
+
+              <input
+                type="text"
+                value={folderName}
+                onChange={(e) => setFolderName(e.target.value)}
+                autoFocus
+              />
+              <div className="buttons">
+                <button className="btn-white" onClick={handleCancel}>
+                  Cancel
+                </button>
+                <button className="btn-white create-btn" onClick={handelCreate}>
+                  Create
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </>
     )
   );
 };
